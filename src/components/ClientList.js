d5a1677d0fc0e164fb09832f8d7cf66a453a1963
Jaime Borges <jeborgesm@users.noreply.github.com>
2025-02-17 00:12:22 -0600

    Update ClientList.js
    Updated to use the Factory design pattern

--- src/components/ClientList.js
+++ src/components/ClientList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Client from './Client';
+import ClientFactory from './ClientFactory';
 
 const ClientList = () => {
     const [clients, setClients] = useState([]);
@@ -20,7 +20,8 @@ const ClientList = () => {
             <h1>Clients</h1>
             <ul>
                 {clients.map(client => (
-                    <Client key={client.id} client={client} />
+                    // Use the factory to create Client components
+                    ClientFactory.createClient('Client', { key: client.id, client })
                 ))}
             </ul>
         </div>
