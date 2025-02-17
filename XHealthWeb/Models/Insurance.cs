bebdf55d41de15f048540d679bbbe05d1f823e4b
Jaime Borges <jeborgesm@users.noreply.github.com>
2025-02-17 02:22:23 -0600

    Update Insurance.cs

--- XHealthWeb/Models/Insurance.cs
+++ XHealthWeb/Models/Insurance.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace XHealthWeb.Models
 {
     public class Insurance
@@ -6,5 +8,8 @@ namespace XHealthWeb.Models
         public string PlanName { get; set; }
         public string Policy { get; set; }
         public string GroupNumber { get; set; }
+
+        // Add this property
+        public ICollection<AccountInsurance> AccountInsurances { get; set; }
     }
 }
