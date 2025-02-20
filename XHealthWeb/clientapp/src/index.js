d733a164d5fc28ea87fb33e78c3c6951e2d9cb7e
Jaime Borges <jeborgesm@hotmail.com>
2025-02-20 00:25:54 -0600

    added index.js to clientapp/src

---
+++ XHealthWeb/clientapp/src/index.js
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PatientsList from './components/PatientsList';
+import AddPatient from './components/AddPatient';
+import EditPatient from './components/EditPatient';
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<PatientsList />} />
+                <Route path="/list" element={<PatientsList />} />
+                <Route path="/add" element={<AddPatient />} />
+                <Route path="/edit/:id" element={<EditPatient />} />
+            </Routes>
+        </Router>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
