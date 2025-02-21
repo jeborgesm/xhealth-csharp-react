9710ec7ed9ae3b7221ea0604330fb3f1ce43f139
Jaime Borges <jeborgesm@hotmail.com>
2025-02-21 00:34:32 -0600

    Added new migration adding email and phone for client

---
+++ XHealthWeb/Migrations/20250221063231_AddEmailAndPhoneToClient.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XHealthWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailAndPhoneToClient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Clients",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Clients",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Clients");
+        }
+    }
+}
